889d9e4a6db4bef6749f739a2b0f9d574f1c026e
Mr. Conrad <62264897+cblashPP@users.noreply.github.com>
2020-10-11 19:04:35 -0700

    First attempt at addressing duplicates
    TOTALLY not optimal or working solution.  Realized these two things partway through.
    Just want to show my thought process on solving this problem.
    Also I'm not too worried on how much time I've spent on this straightforward solution.
    It is my first one after all!

--- multiples_of_3_and_5.py
+++ multiples_of_3_and_5.py
@@ -4,17 +4,30 @@ Created on Tue Oct  6 18:38:36 2020
 
 @author: Mr. Conrad
 """
+"""
+Tried my solution on euler and found it wasn't correct :(
+luckily I listed out all the factors going into the final sum!
+I see now that there are duplicate factors being added
+So all I need to do is 
+remove the dupes from the array
+and add each element to sum after doing so
+"""
+
+'''
+Here's how I can remove those dupes
+They occur when the factor is divisible by 3 and 5 
+but this really just means it's a multiple of 15!
+I can just make some sort of function that goes through the completed array
+and removes at least one copy of each factor of 15
+Alternatively I can have one of the factor loops, probably 5, the shorter one
+conditionally check if p is divisible by 15
+Eventually we'll choose one of these two methods that has the shorter run time
+Specifically as the MAX_VALUE approaches infinity
+Scalability everybody!
+'''
+
+#Let's filter the array on this attempt just because it sounds more difficult
 import numpy as np
-"""
-I'm kind of bored at the moment, and this is my first real practice problem
-It's on this site called project euler, here's the url
-https://projecteuler.net/
-After looking at it I think I solve it, so that's a good sign.
-I think the biggest problem will be putting it in my Github repo.
-The time I made this is at the top of this file
-I wonder how much of a gap there'll be between that time and the commit
-Let's get started shall we!
-"""
 print("Solve the following:")
 print("""
       If we list all the natural numbers below 10 
@@ -24,62 +37,61 @@ print("""
       """)
 #Looking at this I think I just need to make a sum for starters
 sum = 0
-#Now the problem tells me to list the items then it tells me their sum
-#Hmm
-#No I don't think that tells me anything
-#But I do know what to do now!
-
-#I shall have a number that increments, I'll call it 'i'
-i = 0
-#Then I increment why by 1 and multiply it by 3 and 5
-#If I were to put it into a loop the sequence would go something like this
-"""
-i * 3 = p (where p is the product)
-if p < 1000
-then 
-sum += p
-else move on I guess
-
-then do the above for i and 5
-use p again because it's just a place holder for that multiple
-after adding p to the sum we increase i by 1
-
-now the loop should end when 
-that if condition for p is false for both 3 and 5
-since 5 is bigger it'll be false first, when i = 200 to be exact
-3 multiplied by 200 is only 600 though, so we'll be adding in multiples of 3 
-many more times than multiples of 5
-hm
-I know what the amount of multiple of 5 there are by  dividing 1000 by 5
-I can do the same for 3!
-1000 by 3 is 333
-so I can just do two separate loops one counting to 200 and 1 counting to 333!
-"""
 #To make this problem more universal we'll make 1000 variable
 #Then we'll use the value to find limits of each multiple
 MAX_VALUE = 1000
 lim_3 = int(MAX_VALUE / 3)
 lim_5 = int(MAX_VALUE / 5)
+lim_15 = int(MAX_VALUE / 15)
 #I'll also throw the different factors of 3 and 5 in an array and display them
 #Since the loops are separate I'll throw them in, sort, then display
 factors = np.array([])
-#Here's the loop for the multiples of 3
+
+#Loop for the multiples of 3
 for i in range(1, lim_3):
     p = i * 3                   #Calculate the factor
     factors = np.append(factors, p)       #append the factor to the array
-    sum += p                    #add factor to sum
 
 
 #The loop for multiples of 5
+
 for i in range(1, lim_5):
     p = i * 5
     factors = np.append(factors, p)
-    sum += p
+    
+'''
+So here's what I'm thinking
+I read through an online tutorial on filtering NumPy arrays
+I can just make a loop that sets a false boolean on all the multiples of 15
+Then I can add them back in on a 4th loop
+hmm
+but now that I think about having so many different loops is probably super slow
+maybe I'll just go with plan b and just set a conditional in the shorter loop
+For now I'll implement the filtering method for my own experience
+Then come back and address another issue I've found
+'''
+
+#lets set the condition to make the multiples of 15 go to false
+factor_15 = factors % 15 != 0 
+
+#one line sets up the new array
+factors = factors[factor_15]
+
+#now we add back in the factors of 15
+for i in range(1, lim_15):
+    p = i * 15
+    factors = np.append(factors, p)
+
 
 print("The desired factors are as follows: ")
 #sort the array and output it 
 factors = np.sort(factors)
 print(factors)
+
+#now we need to add the factors together
+for x in factors:
+    sum += x
+
 
 print("\n\nSolution:")
 print("The sum of all multiples of 3 and 5 less than 1000 is " + str(sum))
