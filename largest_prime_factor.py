8f5d2d803c0bf29ba654ca69958bc63f1cfc6a8d
Mr. Conrad <62264897+cblashPP@users.noreply.github.com>
2020-10-26 12:09:01 -0700

    Second attempt
    Been working on this problem for too long.  I have a working solution but it sucks if the target number is too big.
    Learned a lot of valuable lessons from this....
    
    I need to remember, now is the time for practice.  Ask questions and look things up. If I don't know the problem, I don't know it.
    Give it my best shot and if I don't know it, that's fine, I'm learning.

--- largest_prime_factor.py
+++ largest_prime_factor.py
@@ -4,63 +4,43 @@ Created on Thu Oct 22 09:49:10 2020
 
 @author: Mr. Conrad
 """
-'''
-Been a while since I did one of these
-Once you start something it's always hardest to remain consistent.
-I've come back though, which is huge considering,
-almost 1 million people completed 'multiples of 3 and 5' 
-but only half that many completed 'largest prime factor,' this problem
-'''
 
 print('''The prime factors of 13195 are 5, 7, 13 and 29.
       
 What is the largest prime factor of the number 600851475143 ?''')
 
 '''
-off the top of my head I know the first few prime numbers
-I'll initiate an array with those first few and continue generating them from there
-but I only need to generate a certain amount of these factors
-normally the largest, whole number, factor is paired with the number 2
-eg. 2 and 50 make 100,  2 is the smallest factor and 50 the largest
-but this only applies to even numbers.
-I'd then try 3 as a factor if it's odd then 5 and so on,
-Through my prime numbers until I get the smallest prime factor.
-Then, knowing the smallest prime factor, I also know the largest non-prime factor
-I can then generate prime numbers between that smallest prime factor and largest non-prime
-then test them against my number
+Picking up where I started now that I can generate prime numbers,
+I just need to figure out how to use it to find the smallest/largest factor
+pairing of whatever number I'm looking for.
 '''
+import numpy as np
+#Number given by problem, find the largest prime factor of it
+TARGET_NUM = 13195
 #start with the first prime number
-prime_nums = [2]
+prime_nums = np.array([2])
 
-def prime_number_gen(num_arr, end_point):
-    #Take one more than the largest number in the list as the Current NUMber
-    c_num = num_arr[-1] + 1
+#prime number generation function
+def get_next_prime_num():
+    #Take one more than the largest prime number in the list as the Current NUMber
+    c_num = prime_nums[-1] + 1
     #check if that number is even
     #if it's even then it's already not a prime number
     if (c_num % 2) == 0:
         c_num += 1
         
-    #loop through all the relevant checking numbers between our start and end inclusive   
-    while c_num <= end_point:
+    #loop through all the relevant checking numbers between our start and end inclusive 
+    found = False
+    while found != True:
         #check the current number versus all prime numbers generated so far
-        found = prime_num_compare(num_arr, c_num)
-        if found:
-            print("Adding " + str(c_num) + " to the array...")
-            num_arr.append(c_num)
-            print(num_arr)
-        c_num += 2
+        if prime_num_compare(c_num): found = True
+        else: c_num += 2
         
-    return num_arr
-'''
-So I thought it over for a long time and I couldn't find a way around a nested loop
-and when I tried to put them together in one function it was more or less falling appart.
-I figured, if all I'm checking if a factor exists in the array I could go with a boolean,
-And it's easier to have it gen'd by another function 
-than to switch it back and forth each iteration in a mess of loops
-Makes more readable
-'''  
-def prime_num_compare(num_arr, c_num):
-        for x in num_arr:
+    return c_num 
+
+#Helper function for prime number generator
+def prime_num_compare(c_num):
+        for x in prime_nums:
             #check if any of the prime factors are factors of the current number
             #factors in between the prime factors are negligible!
             if (c_num % x) == 0:
@@ -68,5 +48,56 @@ def prime_num_compare(num_arr, c_num):
         else:
             return True
 
-#example run of generating all prime numbers between 2 and 21, inclusive
-prime_nums = prime_number_gen(prime_nums, 21)
+#Main body of code
+c_factor = prime_nums[0]
+#generate prime numbers until you find the smallest prime factor of the target
+print("Now searching for smallest prime factor of target number " + str(TARGET_NUM) + "\n\n")
+while (TARGET_NUM % c_factor) != 0:
+    c_factor = get_next_prime_num()
+    #print("Adding " + str(c_factor) + " to list of prime numbers...")
+    prime_nums = np.append(prime_nums, c_factor)
+    #prime_nums = append_prime_num_arr(prime_nums, c_factor)
+    
+print("Prime factor found!\n\n")
+
+
+#use division to obtain the largest factor of the target
+large_factor = int(TARGET_NUM / c_factor)
+small_factor = c_factor
+print("\n\nSmallest prime factor of target number: " + str(small_factor))
+print("Other factor in pairing: " + str(large_factor))
+
+
+#generate all prime numbers between the two known factors
+print("Now generating all prime numbers between factor pairing.... \n\n")
+while c_factor <= large_factor:
+    c_factor = get_next_prime_num()
+    #print("Adding " + str(c_factor) + " to list of prime numbers...")
+    prime_nums = np.append(prime_nums, c_factor)
+    #prime_nums = append_prime_num_arr(prime_nums, c_factor)
+    
+print("Generation Done!\n")
+print("Known prime numbers:")
+print(prime_nums)
+'''
+Now our goal is to check each prime number as a factor 
+vs. the target number starting from the largest prime numbers going to the smallest.
+eventually it might even hit our smallest prime factor
+'''
+#create index to be decremented on for search
+d = -1
+#set the current factor to the end of the prime number list, the largest prime number
+c_factor = prime_nums[d]
+#check if the current prime number is a factor of the target number
+print("\n\nNow locating largest prime factor of target number " + str(TARGET_NUM) + " from available prime numbers:")
+while (TARGET_NUM % c_factor) != 0:
+    d -= 1
+    c_factor = prime_nums[d]
+    #print('.', end='')
+
+#once a factor is found display it as the solution
+print('')
+print("Largest Prime factor found!\n\n")
+l_prime_factor = c_factor     
+print("Solution:")
+print("The largest prime factor of " + str(TARGET_NUM) + " is " + str(l_prime_factor))
